3055db91b1f1bee5cb9df7b6aa8963f346c799c9
Leo Mosconi <106116270+Mosk09@users.noreply.github.com>
2022-12-26 11:38:20 -0300

    Update LinkedIn.js

--- pages/LinkedIn.js
+++ pages/LinkedIn.js
@@ -1,18 +1,1 @@
-import Layout from "../components/Layout";
 
-export default function LinkedIn() {
-  return (
-    <Layout>
-      <div className="row">
-        <div className="col-md-4 offset-md-4">
-          <div className="card card-bdy text-center">
-            <a href="https://www.linkedin.com/in/leo-mosconi-09ab27230/">
-              <h1 className="text-center">LinkedIn</h1>
-            </a>
-            <img src="/LinkedIn.png"></img>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
-}
