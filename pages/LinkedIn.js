80c2f022f96a18884fb7a1eb2791431eb973534b
Mosk09 <leomosconi9@gmail.com>
2023-01-30 14:53:24 -0300

    actualizado

--- pages/LinkedIn.js
+++ pages/LinkedIn.js
@@ -1,1 +1,8 @@
 
+import React from 'react'
+
+export default function github() {
+  return (
+    <a href='https://www.linkedin.com/in/leo-mosconi-fullstackweb/'>LinkedIn</a>
+  )
+}
--- pages/github.js
+++ pages/github.js
@@ -1,1 +1,8 @@
 
+import React from 'react'
+
+export default function github() {
+  return (
+    <a href='https://github.com/Mosk09'>GitHub</a>
+  )
+}
